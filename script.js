ae397789af286cd2d3d3c281285073dc4d4ea731
maddygbutler <madelinebutler@students.berkeley.netgit confH205-MM24:~ h205p5$ git config --global user.email madelinebutler@studeeley.net>
2018-12-12 13:48:36 -0800

    Final Grade Calculator

--- script.js
+++ script.js
@@ -29,9 +29,84 @@ function currentGrade(){
     console.log(projectsPoints);
     console.log(projectsWeight);
 
-    var hwArr = newNumber(homeworkPoints);
-    console.log(hwArr);
+    var hwAvg = average(newNumber(homeworkPoints));
+    console.log(hwAvg);
 
+    var parAvg = average(newNumber(participationPoints));
+    console.log(parAvg);
+
+    var cwAvg = average(newNumber(classworkPoints));
+    console.log(cwAvg);
+
+    var testAvg = average(newNumber(testsPoints));
+    console.log(testAvg);
+
+    var proAvg = average(newNumber(projectsPoints));
+    console.log(proAvg);
+
+    parseInt(homeworkWeight);
+
+    parseInt(participationWeight);
+
+    parseInt(classworkWeight);
+
+    parseInt(testsWeight);
+
+    parseInt(projectsWeight);
+
+    var hwTotal= (hwAvg*(homeworkWeight/100));
+    console.log(hwTotal);
+
+    var parTotal= (parAvg*(participationWeight/100));
+    console.log(parTotal);
+
+    var cwTotal=(cwAvg*(classworkWeight/100));
+    console.log(cwTotal);
+
+    var testTotal= (testAvg*(testsWeight/100));
+    console.log(testTotal);
+
+    var proTotal= (proAvg*(projectsWeight/100));
+    console.log(proAvg);
+
+    var currentGrade=(hwTotal+parTotal+cwTotal+testTotal+proTotal);
+
+    console.log(currentGrade);
+
+    document.getElementById("currentGrade").innerHTML= currentGrade;
+
+    return currentGrade;
+}
+
+function gradeNeeded(){
+    var gradeDesired = [];
+
+    var finalWeight = [];
+
+    var current= currentGrade();
+
+    var finalGrade = current;
+
+    gradeDesired.push(document.getElementById("gradeDesired").value);
+    finalWeight.push(document.getElementById("finalWeight").value);
+
+    gradeDesired = parseInt(gradeDesired);
+    finalWeight = parseInt(finalWeight);
+
+    var currentPoints = (finalGrade / 100) * ( 1 - (finalWeight / 100));
+
+    console.log(currentPoints);
+
+    var pointsNeeded = (gradeDesired / 100 ) - currentPoints;
+    var gradeNeeded = (pointsNeeded / (finalWeight/100)) * 100;
+
+    gradeNeeded = gradeNeeded*100;
+    gradeNeeded = Math.round(gradeNeeded);
+    gradeNeeded = gradeNeeded/100;
+
+    document.getElementById("youNeed").innerHTML= "You will need to score a "+ gradeNeeded + "%" + "to get a " + gradeDesired + "% overall";
+
+    console.log(gradeNeeded);
 }
 
 
@@ -56,3 +131,8 @@ function average(arr){
     }
     return sum/(arr.length);
 }
+
+function findWeight(){
+
+}
+
